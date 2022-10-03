27d66c92480bf0ef87bb81cda870106de1547572
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2022-10-03 17:30:03 -0400

    done

--- app.js
+++ app.js
@@ -19,7 +19,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers.hbs', { beersFromApi })
+    })
+    .catch(error => console.log(error));
+
+})
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer.hbs', { responseFromAPI })
+    })
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
